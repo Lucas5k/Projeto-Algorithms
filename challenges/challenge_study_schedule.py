8dd36b6da17c844cfb558a3b3ba9be83bc8a890c
Lucas5k <lucascabral112347@gmail.com>
2022-11-21 17:03:06 -0300

    feat: implementação do req 1, feito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if not isinstance(target_time, int) or not target_time:
+        return None
+    for start, end in permanence_period:
+        if not isinstance(start, int) or not isinstance(end, int):
+            return None
+        if start <= target_time <= end:
+            count += 1
+    return count
