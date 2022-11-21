2c7072e8bb39df2bf4e55c995df51fd96e506881
Lucas5k <lucascabral112347@gmail.com>
2022-11-21 17:29:57 -0300

    feat: implementação do encrypt

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    testing = encrypt_message("AABBCC", 3)
+    assert testing == "BAA_CCB"
+    testing2 = encrypt_message("ABBCCA", 4)
+    assert testing2 == "AC_CBBA"
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("AABBCC", "OLA")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(8,8)
