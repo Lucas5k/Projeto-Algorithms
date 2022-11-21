ef2adebdc9a93f79ea151e41bba99b1d3d424f1d
Lucas5k <lucascabral112347@gmail.com>
2022-11-21 17:48:37 -0300

    fix: arrumando lint

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -7,4 +7,5 @@ def is_palindrome_recursive(word, low_index, high_index):
         return True
     if word[low_index] != word[high_index]:
         return False
+
     return is_palindrome_recursive(word, low_index + 1, high_index - 1)
