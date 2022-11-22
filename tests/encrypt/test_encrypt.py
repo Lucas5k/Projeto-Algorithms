1db55dcbf950c6bee71e8f9e9e093914f02d5f62
Lucas5k <lucascabral112347@gmail.com>
2022-11-22 16:14:28 -0300

    refactor: arrumando o teste, tirando as variaveis e colocando tudo no assert

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -5,6 +5,7 @@ import pytest
 def test_encrypt_message():
     assert encrypt_message("AABBCC", 3) == "BAA_CCB"
     assert encrypt_message("ABBCCA", 4) == "AC_CBBA"
+    assert encrypt_message("AABBCC", -1) == "CCBBAA"
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("AABBCC", "OLA")
     with pytest.raises(TypeError, match="tipo inválido para message"):
